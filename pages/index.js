c6a12a34c73d67a46bd218866e0c6d17e3a64b17
Heemank Verma <vheemank@gmail.com>
2021-08-12 03:08:14 +0530

    fixing

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable react/jsx-no-comment-textnodes */
 
 import Head from 'next/head'
 // import Image from 'next/image'
@@ -55,16 +57,7 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        Here is a basic footer
       </footer>
     </div>
   )
